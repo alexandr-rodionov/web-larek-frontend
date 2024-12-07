0d25defb6c7caca62b35163e46bf94cd5ca7606d
Alexandr Rodionov <rodya3003@yandex.ru>
2024-12-07 23:26:26 +0300

    bugfix/main logic, card, model

---
+++ src/components/Basket.ts
@@ -0,0 +1,42 @@
+import { createElement, ensureElement, formatNumber } from "../utils/utils";
+import { Component } from "./base/component";
+import { IEvents } from "./base/events";
+
+interface IBasket {
+  items: HTMLElement[];
+  total: number;
+}
+
+export class Basket extends Component<IBasket> {
+  protected _list: HTMLElement;
+  protected _total: HTMLElement;
+  protected _button: HTMLButtonElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+
+    this._list = ensureElement<HTMLElement>('.basket__list', container);
+    this._total = ensureElement<HTMLElement>('.basket__price', container);
+    this._button = ensureElement<HTMLButtonElement>('.basket__button', container);
+    if(this._button)
+      this._button.addEventListener('click', () => { events.emit('order:open') });
+    this.items = [];
+  }
+
+  set items(items: HTMLElement[]) {
+    if(items.length) {
+      this._list.replaceChildren(...items);
+      this.setDisabled(this._button, false);
+    } else {
+      this._list.replaceChildren(createElement<HTMLElement>('div', {
+        className: 'basket__empty',
+        textContent: 'В корзине пусто :('
+      }));
+      this.setDisabled(this._button, true);
+    }
+  }
+
+  set total(value: number) {
+    this.setText(this._total, `${formatNumber(value)} синапсов`);
+  }
+}
